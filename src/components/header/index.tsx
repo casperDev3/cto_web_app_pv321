fe1ee3af725c0dbbbdda4aa9645c2dbd5a8894f1
Ihor Lialiuk <ihor.lialiuk@gmail.com>
2025-02-03 19:03:07 +0200

    add header & footer

---
+++ src/components/header/index.tsx
@@ -0,0 +1,24 @@
+const Header = () => {
+    return (
+        <>
+            <header>
+                <h2>Header</h2>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/blog">Blog</a>
+                        </li>
+                        <li>
+                            <a href="/contact">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+}
+
+export default Header;
